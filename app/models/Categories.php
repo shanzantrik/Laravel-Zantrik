011f9939516667231706af75f7c6c33fbf42f7cd
Shantanu Goswami <shantanu@zantrik.in>
2014-08-12 18:30:41 +0530

    Create Categories.php

---
+++ app/models/Categories.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
+
+class Categories extends Eloquent implements UserInterface, RemindableInterface {
+
+	use UserTrait, RemindableTrait;
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'categories';
+
+	protected $guarded = array('id');
+
+	protected $fillable = array('name', 'slug', 'parent_id');
+}
