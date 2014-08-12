2718cb2ae94f9d78d43500fe2419c5f77415bdd8
Shantanu Goswami <shantanu@zantrik.in>
2014-08-12 18:24:38 +0530

    Create header.blade.php

---
+++ app/views/admin/category/header.blade.php
@@ -0,0 +1,4 @@
+<ul class="nav navbar-nav" style="width: 100%;">
+	<li class="active">{{ HTML::linkRoute('category.list', 'List') }}</li>
+	<li>{{ HTML::linkRoute('category.create', 'Create') }}</li>
+</ul><br /><br />
